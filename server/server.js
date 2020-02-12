6ab237e399d15a753f8cf5f1f18964c207cbf686
princenuada2019 <43705327+princenuada2019@users.noreply.github.com>
2020-02-12 19:21:41 +0330

    server setup and database restructured

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoos');
+var {Todo} = require('./models/todo');
+var {Users} = require('./models/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed,
+        completedAt: req.body.completedAt
+    });
+
+    todo.save().then((savedTodo) => {
+        res.send(savedTodo);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Starting on port 3000')
+});
