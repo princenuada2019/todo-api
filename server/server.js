2130da010feeb5d1a46f62287f61945b0e88220e
princenuada2019 <43705327+princenuada2019@users.noreply.github.com>
2020-02-18 13:57:57 +0330

    add PCTCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -60,6 +61,28 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error);
     })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('User id is invalid');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('todo not found');
+        }
+        res.send({todo});
+    }).catch((error) => res.status(400).send(error));
 });
 
 app.listen(3000, () => {
