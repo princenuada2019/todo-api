e158322b83cd0ba09129fede9198b246dd013b8e
princenuada2019 <43705327+princenuada2019@users.noreply.github.com>
2020-02-25 19:57:14 +0330

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '053122ali//';
+
+bcrypt.genSalt(10, (error, salt) => {
+    bcrypt.hash(password, salt, (error, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$ma3T.aK6QnysqbsGwMAewuuQYXWjDB2P2j.RXpF.5QzmCGY6RC4qq';
+
+bcrypt.compare(password, hashedPassword, (error, res) => {
+    console.log(res);
+});
+
+/*
 let user = {
     id: 13
 };
@@ -10,6 +26,7 @@ console.log('token', token);
 
 let decoded = jwt.verify(token, 'aliisagoodboylovecoding,butnotblive');
 console.log('decoded', decoded);
+*/
 
 /*
 
